9a70e7c38d14242810bf005ba83bac69b886eec5
David Peach <mail@davidpeach.co.uk>
2021-06-11 20:50:11 +0100

    Initial PHP test scaffolding - only adds new base testcase with helper method at the moment.

---
+++ src/Commands/ScaffoldPhpTesting.php
@@ -0,0 +1,26 @@
+<?php
+namespace DavidPeach\EscAppScaffolder\Commands;
+
+use DavidPeach\BaseCommand\StepBinary;
+use Symfony\Component\Process\Process;
+
+class ScaffoldPhpTesting extends StepBinary
+{
+    public function question()
+    {
+        return "Would you like to setup PHP Tests for this project?";
+    }
+
+    public function handle()
+    {
+        $this->createEscTestCaseFile();
+    }
+
+    private function createEscTestCaseFile()
+    {
+        file_put_contents(
+            base_path('tests/EscTestCase.php'),
+            file_get_contents(__DIR__ . '/../stubs/EscTestCase.stub')
+        );
+    }
+}
